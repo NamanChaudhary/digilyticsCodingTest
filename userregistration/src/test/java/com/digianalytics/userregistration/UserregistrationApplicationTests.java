a31466f8ab80d51d5c8271ad717df8c0896afdb9
namanchaudhary <naman.kr056@gmail.com>
2020-06-01 23:11:02 +0530

    Project stracuture

---
+++ userregistration/src/test/java/com/digianalytics/userregistration/UserregistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digianalytics.userregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserregistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
